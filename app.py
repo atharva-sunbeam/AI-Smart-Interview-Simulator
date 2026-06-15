70a0022c213a7c73e99cccc71472f95aceb633d5
atharva-birje-sunbeam <atharvapersonal234@gmail.com>
2026-06-15 23:08:28 +0530

    Project's first commit

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="AI Smart Interview Simulator",
+    page_icon="🎯"
+)
+
+st.title("🎯 AI Smart Interview Simulator")
+
+st.write("Project setup successful.")
