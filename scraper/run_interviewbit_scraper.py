c827126305c7c99f023739307d94832af0ba2dda
atharva-birje-sunbeam <atharvapersonal234@gmail.com>
2026-06-19 23:08:14 +0530

    Implemented InterviewBit scraper prototype

---
+++ scraper/run_interviewbit_scraper.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+
+from scraper.interviewbit_scraper import (
+InterviewBitScraper
+)
+
+def main():
+    scraper = InterviewBitScraper()
+
+    questions = scraper.fetch_questions()
+
+    output_dir = Path(
+        "datasets/raw"
+    )
+
+    output_dir.mkdir(
+        parents=True,
+        exist_ok=True
+    )
+
+    output_file = (
+        output_dir /
+        "python_questions.txt"
+    )
+
+    data = "\n".join(questions)
+
+    scraper.save_to_file(
+        output_file,
+        data
+    )
+
+    print(
+        f"Saved {len(questions)} "
+        f"questions to {output_file}"
+    )
+
+
+if __name__ == "__main__":
+    main()
